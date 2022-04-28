b0f8e67a0a37d8d0c13216f0965485ea8794c663
Son Dang <sondnpt00343@gmail.com>
2022-04-28 11:29:21 +0700

    Bài 60: Cài đặt và cấu hình prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import logo from './logo.svg';
-import Button from '~/components/Button'
+import Button from '~/components/Button';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Button />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
-import myAlert from '~/myAlert'
+import myAlert from '~/myAlert';
 
 function Button() {
-    return <button onClick={myAlert}>Click me!</button>
+    return <button onClick={myAlert}>Click me!</button>;
 }
 
-export default Button
+export default Button;
